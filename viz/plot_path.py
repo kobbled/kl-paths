ac3ab2ebce62f7889c1bc61dfacaddbbdc7f0cee
kobbled <mattpdewar@gmail.com>
2021-04-01 01:32:47 -0600

    add tests for paths abstraction class
    -`defaultpath.klt` is an example template file for the class.
    -tests for a planar shape, a OD bore, and ID bore were added.
    
    ..warning:: make sure register lists, and user/tool frames reflect the workcell before running. You need to update `defaultpath.klt`, `test_env.tpp`, and frame references in `test_pth_mtn.kl`

--- viz/plot_path.py
+++ viz/plot_path.py
@@ -68,8 +68,8 @@ TOOLPATH_GROUP = '1'
 # PROG_NAME = 'AutoProgram'
 
 #cell2
-# REF_FRAME = 'cut_frame'
-REF_FRAME = 'vert_frame'
+REF_FRAME = 'cut_frame'
+# REF_FRAME = 'vert_frame'
 TOOL_FRAME = '756mm_Tool'
 PART_NAME = 'path'
 PROG_NAME = 'AutoProgram'
